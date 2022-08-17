e82e3839ac44f2af947a7bf431405e5258448fdd
solarTribute <solartribute@gmail.com>
2022-08-17 19:05:44 +0300

    exclude DataSourceAutoConfiguration

--- src/main/java/com/example/brunch/BrunchApplication.java
+++ src/main/java/com/example/brunch/BrunchApplication.java
@@ -1,7 +1,11 @@
 package com.example.brunch;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
 
 @SpringBootApplication
 public class BrunchApplication {
@@ -11,3 +15,4 @@ public class BrunchApplication {
 	}
 
 }
+
