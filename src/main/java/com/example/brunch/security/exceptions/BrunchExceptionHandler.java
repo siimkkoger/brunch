8181b43500c65874a9bbdce4899799116fe1b19c
Siim Karel Koger <siimkkoger@gmail.com>
2022-09-09 15:07:12 +0300

    Clean up BrunchExceptionHandler

--- src/main/java/com/example/brunch/security/exceptions/BrunchExceptionHandler.java
+++ src/main/java/com/example/brunch/security/exceptions/BrunchExceptionHandler.java
@@ -3,13 +3,12 @@ package com.example.brunch.security.exceptions;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import org.springframework.web.util.WebUtils;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
@@ -21,47 +20,46 @@ public class BrunchExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(BrunchException.class)
     public ResponseEntity<Object> handleBrunchException(final BrunchException exception, final WebRequest request) {
         final HttpHeaders headers = new HttpHeaders();
-        final ErrorMessage errorMessage = buildErrorMessageBody(exception, request);
+        final ErrorMessage errorMessage = buildErrorMessageBody(exception.getStatusCode(), exception.getDescription(), request);
         return handleExceptionInternal(exception, errorMessage, headers, exception.getStatusCode(), request);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<ErrorMessage> handleConstraintViolation(final ConstraintViolationException ex, final WebRequest request) {
+    public ResponseEntity<Object> handleConstraintViolation(final ConstraintViolationException ex, final WebRequest request) {
         final StringBuilder builder = new StringBuilder();
         builder.append("Constraint violations: [\n");
         for (final ConstraintViolation<?> violation : ex.getConstraintViolations()) {
             builder.append(violation.getPropertyPath()).append(" ").append(violation.getMessage()).append("\n");
         }
         builder.append("]");
-        return buildResponseEntity(new BrunchException(BrunchException.Type.BAD_REQUEST, builder.toString()), request);
+
+        final HttpHeaders headers = new HttpHeaders();
+        final BrunchException exception = new BrunchException(BrunchException.Type.BAD_REQUEST, builder.toString());
+        final ErrorMessage errorMessage = buildErrorMessageBody(exception.getStatusCode(), exception.getDescription(), request);
+        return handleExceptionInternal(exception, errorMessage, headers, exception.getStatusCode(), request);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorMessage> globalExceptionHandler(Exception ex, WebRequest request) {
-        return buildResponseEntity(new BrunchException(BrunchException.Type.SERVER_ERROR, ex.getMessage()), request);
+    public ResponseEntity<Object> globalExceptionHandler(Exception ex, WebRequest request) {
+        final HttpHeaders headers = new HttpHeaders();
+        final BrunchException exception = new BrunchException(BrunchException.Type.SERVER_ERROR, ex.getMessage());
+        final ErrorMessage errorMessage = buildErrorMessageBody(exception.getStatusCode(), exception.getDescription(), request);
+        return handleExceptionInternal(exception, errorMessage, headers, exception.getStatusCode(), request);
     }
 
     @Override
-    protected ResponseEntity<Object> handleExceptionInternal(
-            Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return new ResponseEntity<>("Something...", status);
+    protected ResponseEntity<Object> handleNoHandlerFoundException(
+            final NoHandlerFoundException ex, final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+        final ErrorMessage body = buildErrorMessageBody(HttpStatus.NOT_FOUND, "Handler not found!", request);
+        return handleExceptionInternal(ex, body, headers, status, request);
     }
 
-    private ErrorMessage buildErrorMessageBody(final BrunchException exception, final WebRequest request) {
+    private ErrorMessage buildErrorMessageBody(final HttpStatus httpStatus, final String errorMessage, final WebRequest request) {
         return new ErrorMessage(
                 new Date(),
-                exception.getStatusCode().value(),
-                exception.getDescription(),
-                request.getDescription(false));
-    }
-
-    private ResponseEntity<ErrorMessage> buildResponseEntity(final BrunchException exception, final WebRequest request) {
-        final ErrorMessage message = new ErrorMessage(
-                new Date(),
-                exception.getStatusCode().value(),
-                exception.getDescription(),
-                request.getDescription(false));
-        return new ResponseEntity<>(message, exception.getStatusCode());
+                httpStatus.value(),
+                errorMessage,
+                ((ServletWebRequest)request).getRequest().getRequestURI());
     }
 
 }
--- src/main/java/com/example/brunch/security/exceptions/ErrorMessage.java
+++ src/main/java/com/example/brunch/security/exceptions/ErrorMessage.java
@@ -2,5 +2,5 @@ package com.example.brunch.security.exceptions;
 
 import java.util.Date;
 
-public record ErrorMessage(Date timestamp, int status, String error, String path) {
+public record ErrorMessage(Date timestamp, int status, String errorMessage, String path) {
 }
